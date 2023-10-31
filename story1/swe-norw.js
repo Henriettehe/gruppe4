fd8fb3fbeeb0630a5a992752fe7a72603f8011aa
HenrietteH <Henriettehestsveen@gmail.com>
2023-10-31 17:18:33 +0100

    test-nytt dataset 👍norge vs sverige

---
+++ story1/swe-norw.js
@@ -0,0 +1,21 @@
+// your-chart-data.js
+const ctx = document.getElementById('mestSnus').getContext('2d');
+
+const labels = Utils.months({count: 7});
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Svergie VS Norge',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+  };
+
+  new Chart(ctx, config);
